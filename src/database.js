11ff4500bb56ec7f2be3304dafcae12cad680b61
Sergio Díaz <codediaz@outlook.es>
2021-09-03 18:09:57 -0500

    Database credentials conected

--- src/database.js
+++ src/database.js
@@ -1,5 +1,10 @@
 import mongoose from 'mongoose'
 
-mongoose.connect("mongodb://localhost/companydb")
+mongoose.connect("mongodb://localhost/companydb",{
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+
+
+})
     .then(db=>console.log('Db is connected'))
     .catch(error=> console.log(error))
