62d70612e8b24c946114dc89a388ac39c971fd31
Sergio Díaz <codediaz@outlook.es>
2021-09-03 18:40:33 -0500

    Products inserted in mongodb

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express()
 
 app.set('pkg',pkg);
 app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/', (req,res)=> {
     res.json({
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,6 +1,15 @@
+import Product from '../models/Product'
 
+export const createProduct= async(req,res)=>{
+    //console.log(req.body)
 
-export const createProduct=(req,res)=>{
+    const{name,category,price,imgURL}= req.body //Para que todos tengan req.body
+
+    const newProduct = new Product({name,category,price,imgURL}) //definimos
+
+   const productSaved = await newProduct.save()
+
+    res.status(201).json(productSaved)
 
 }
 
