5220c2241d5daba8ee30e9814ccbc16b58abb790
Sergio Díaz <codediaz@outlook.es>
2021-09-03 15:36:57 -0500

    Funtions defined for next connection with database

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,25 @@
+
+
+export const createProduct=(req,res)=>{
+
+}
+
+export const getProducts=(req,res)=>{
+
+}
+
+export const getProductById=(req,res)=>{
+
+}
+
+export const getProducts=(req,res)=>{
+
+}
+
+export const updateProductById=(req,res)=>{
+
+}
+
+export const deleteProductById=(req,res)=>{
+
+}
