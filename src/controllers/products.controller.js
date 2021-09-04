4a9c1348b5f0b109b7b7d11b52643fb951c9d850
Sergio Díaz <codediaz@outlook.es>
2021-09-04 10:22:50 -0500

    Find products by id

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -19,15 +19,17 @@ export const getProducts=async(req,res)=>{
 
 }
 
-export const getProductById=(req,res)=>{
+export const getProductById= async(req,res)=>{
+   const product= await Product.findById(req.params.productId)
+   res.status(200).json(product)
 
 }
 
 
-export const updateProductById=(req,res)=>{
+export const updateProductById= async(req,res)=>{
 
 }
 
-export const deleteProductById=(req,res)=>{
+export const deleteProductById= async(req,res)=>{
 
 }
