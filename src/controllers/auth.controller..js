cf2691f61b6a85370b4134e47f96c5aecde0f6c1
Sergio Díaz <codediaz@outlook.es>
2021-09-05 16:19:28 -0500

    Rename Auth controller

--- src/controllers/auth.controller..js
+++
@@ -1,5 +0,0 @@
-import User from '../models/User'
-
-export const signup = async(req, res) => {}
-
-export const signin = async(req,res) =>{}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 const router = Router()
 
+import * as authController from '../controllers/auth.controller'
+
 router.post('/signup')
 router.post('/signin')
 export default router
